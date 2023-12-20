c9901212a65632efd1b43407a08b4dadd4cd4a3a
V3dant <gameexperts0099@gmail.com>
2023-12-20 18:51:52 +0530

    CSS Changes

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ checkval.addEventListener("click", function(){
         if (tries > 0) {
             if(userInput.value == random_var){
                 rv.innerHTML = "Correct Answer!!<br>You win!!"
-                rv.style.cssText = "color: #fff; font-size: 50px; text-align: center;"
+                rv.style.cssText = "color: #fff; font-size: 45px; text-align: center;"
                 checkval.disabled = true;
                 retry.innerText = "Play Again?";
                 retry.style = "width:100px";
@@ -45,25 +45,25 @@ checkval.addEventListener("click", function(){
             }
             else if(userInput.value < random_var){
                 rv.innerHTML = "Enter Greater Value <br> Try Again!! " + tries-- + " Chance Left";
-                rv.style.cssText = "color: #fff; font-size: 50px; text-align: center;";
+                rv.style.cssText = "color: #fff; font-size: 45px; text-align: center;"
                 userInput.value = ""
             }
             else if(userInput.value > random_var){
                 rv.innerHTML = "Enter Smaller Value <br> Try Again!! " + tries-- + " Chance Left";
-                rv.style.cssText = "color: #fff; font-size: 50px; text-align: center;";
+                rv.style.cssText = "color: #fff; font-size: 45px; text-align: center;"
                 userInput.value = ""
             }
         }
         else if(tries == 0 && userInput.value == random_var){
             rv.innerHTML = "Correct Answer!!<br>You win!!"
-            rv.style.cssText = "color: #fff; font-size: 50px; text-align: center;"
+            rv.style.cssText = "color: #fff; font-size: 45px; text-align: center;"
             checkval.disabled = true;
             retry.innerText = "Play Again?";
             retry.style = "width:100px";
         }
         else{
             rv.innerHTML = random_var + " is correct answer<br>You Lose!!"
-            rv.style.cssText = "color: #fff; font-size: 50px; text-align: center;"
+            rv.style.cssText = "color: #fff; font-size: 45px; text-align: center;"
             retry.innerText = "Play Again?";
             retry.style = "width:100px";
             checkval.disabled = true;
@@ -73,10 +73,10 @@ checkval.addEventListener("click", function(){
     else{
         if (flag === false) {
             rv.innerHTML = "Choose which mode you want to play!";
-            rv.style.cssText = "color: #fff; font-size: 50px; text-align: center;"
+            rv.style.cssText = "color: #fff; font-size: 45px; text-align: center;"
         } else {   
             rv.innerHTML = "Enter Valid Input";
-            rv.style.cssText = "color: #fff; font-size: 50px; text-align: center;"
+            rv.style.cssText = "color: #fff; font-size: 45px; text-align: center;"
         }
     }
 });
